86c8a057cf25e84f548c1466259b64775f896fb0
Stress-ED <johnsonmoyosoremmj@gmail.com>
2025-08-20 16:05:57 +0100

    Add files via upload

---
+++ My first game.py
@@ -0,0 +1,14 @@
+print("Welcome to the best game ever\nMade by Moyo.")
+sc_1= input("You and your friend wash up on the shores of an island after a boat crash.You guys are the only survivors with nothing to eat\nChoose A or B?\nA. Kill and eat your friend\nB. Starve to death\n__")
+if sc_1 == "A":
+    print("Sorry you are too weak, your friend has killed you.Game over")
+else:
+    print("You chose to starve, your friend decides to explore the Island a little and you decided to wait just incase.\nWhile starving, you sight a boat in the distance")
+    sc_2= input("Choose A or B?\nA. Call out to the boat\nB. Keep starving\n__")
+    if sc_2== "A":
+        print("You chose to call out to the boat.The boat sees you by some miracle and is on its way, your friend comes back with a handful of berries and you guys share it while waiting for the boat.\nThe boat arrives and it turns out to be gay pirates that rape you and your friend and kill you both.Game over")
+    else:
+        sc_3= input("You chose not to call out to the boat, your friend comes back from exploring the island and tells you how he only checked out the jungle seemed to go on forever and it should be a pretty big Island but he did find a handful of berries in it\nChoose A or B?\nA. Kill your friend and take all the berries for yourself\nB. Share the berries with him\n__")
+        if sc_3 == "A":
+            print("You successfully killed your friend ate all the berries and set out further into the island")
+            input("In the jungle of the Island")
